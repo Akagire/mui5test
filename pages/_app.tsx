7b78bf5855de1bdb9aa97b462186758f4625fcc8
Takuya Eguchi <seal2501@gmail.com>
2021-10-18 11:24:36 +0900

    initial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
+import makeStyles from '@mui/styles/makeStyles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import { muiTheme } from '../utils/muiTheme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+  return(
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={muiTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
+};
+
+export default MyApp;
---
+++ utils/muiTheme.ts
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material/styles';
+
+export const muiTheme = createTheme({
+  palette: {
+    primary: {
+      light: '#A7B3EB',
+      main: '#38488C',
+      dark: '#1E2B63',
+      disabled: '#ffffff',
+      background: '#aaaaaa'
+    },
+    background: {
+      
+    }
+  }
+});
